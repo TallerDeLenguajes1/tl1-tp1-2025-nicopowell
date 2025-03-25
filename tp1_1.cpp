e7746afcab0b5a5cbecc1e8aa9900d8a77c3a55f
Nicolas Powell <nicopowell30@gmail.com>
2025-03-25 13:08:43 -0300

    se creo un puntero que apunta a un int y se mostraron diferentes datos

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hola mundo");
+  printf("Hola mundo\n");
+  
+  int num = 21, *punt = &num;
+
+  printf("\nContenido del puntero: %d", *punt);
+  printf("\nContenido del puntero: %p", punt);
+  printf("\nContenido del puntero: %p", &num);
+  printf("\nContenido del puntero: %d", sizeof(num));
 
   return 0;
 }
