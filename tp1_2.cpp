ceab7750c472dab6a876271759d4bc30fd9df645
Nicolas Powell <nicopowell30@gmail.com>
2025-03-25 13:34:53 -0300

    se creo la funcion para mostrar la direccion y el contenido de una variable

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -8,7 +8,32 @@ void cuadradoConPuntero(int *punt){
   *punt = *punt * *punt;
 }
 
-int main() {
+void mostrarDireccionYContenido(int *punt){
+  printf("\n\tDireccion de la variable: %p", punt);
+  printf("\n\tContenido de la variable: %d", *punt);
+}
 
+int main() {
+  int num1, num2;
+  printf("Ingrese el primer numero: ");
+  scanf("%d", &num1);
+  printf("Ingrese el segundo numero: ");
+  scanf("%d", &num2);
+
+  printf("\n-- Elevar al cuadrado con funcion tipo int --");
+  printf("\n\tCuadrado de %d = %d", num1, cuadrado(num1));
+  printf("\n\tCuadrado de %d = %d", num2, cuadrado(num2));
+
+  printf("\n\n-- Elevar al cuadrado con funcion tipo void --");
+  cuadradoConPuntero(&num1);
+  cuadradoConPuntero(&num2);
+  printf("\n\tCuadrado de num1 = %d", num1);
+  printf("\n\tCuadrado de num2 = %d", num2);
+
+  printf("\n\n-- Numero 1 --");
+  mostrarDireccionYContenido(&num1);
+
+  printf("\n\n-- Numero 2 --");
+  mostrarDireccionYContenido(&num2);
   return 1;
 }
